380bdc65817fc1becf24479401a04bd4ad06cc73
Jamezjaz <jamezjaz@gmail.com>
2020-10-22 04:55:49 -0500

    docs: sets up the navbar

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+const calculator = (() => {
+  const header = document.getElementById('header');
+  header.innerHTML = `
+                    <nav class="navbar navbar-light header">
+                      <a class="navbar-brand text-light" href="#">
+                        <i class="fas fa-repeat p-2"></i>
+                          Color Flipper
+                        </a>
+                      <a class="navbar-brand text-light" href="#">
+                        <span class="px-4">Jamezjaz</span
+                        <span>Simple Project</span
+                      </a>
+                    </nav>
+                    `;
+})();
