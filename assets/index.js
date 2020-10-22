e8507a9f58b8a92b7b70b50c4b764678d32ae9b2
Jamezjaz <jamezjaz@gmail.com>
2020-10-22 06:10:18 -0500

    docs: implements getRandomNumber function

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,8 @@
-const colors = ['red', 'blue', 'green', 'purple', 'yellow', '#f15025', 'indigo', 'black', 'rgba(255, 133, 200)'];
+const colors = ['red', 'blue', 'green', 'purple', 'yellow', '#F15025', 
+                'indigo', 'rgba(255, 133, 200)', 
+                'rgb(255,215,0)', 'rgb(255,165,0)', '#808000', 
+                '#000000', '#FFFFFF', '#FF00FF'
+                ];
 
 const calculator = (() => {
   const header = document.getElementById('header');
@@ -16,3 +20,16 @@ const calculator = (() => {
                     `;
 })();
 
+const main = (() => {
+  const button = document.getElementById('btn');
+  const color = document.querySelector('.color')
+  button.addEventListener('click', () => {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+  })
+})();
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length);
+}
